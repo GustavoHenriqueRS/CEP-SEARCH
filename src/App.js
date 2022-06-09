8e1e858e81fbc5ab03556ba38dbc232f1ff81ed6
GustavoHenrique23 <gustavohenriqueprivado@gmail.com>
2022-06-09 19:28:04 -0300

    segundo commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div className="container">
+      <>
+        <div>
+          <h1 className="title">GUSTAVO HENRIQUE</h1>
+          <div className="containerInput">
+            <input type = "text" placeholder="Digite seu cep..."/>
+          </div>
+        </div>
+      </>
+    </div>
+  );
+}
+
+export default App;
